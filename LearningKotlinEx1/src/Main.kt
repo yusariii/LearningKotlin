1bf707e192118c57ebe32febb4f4b80e5f497038
Yusariii <khailenguyen2004@gmail.com>
2025-09-30 17:23:23 +0700

    Fix 1 chut

--- LearningKotlinEx1/src/Main.kt
+++ LearningKotlinEx1/src/Main.kt
@@ -1,7 +1,3 @@
-import java.util.Scanner
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     println("Nhập số phân số: ")
     val n = readln().toInt()
